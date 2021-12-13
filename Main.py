33ff68ea044beb3d46750bdc5a25f1bec3254167
Pandiviris <josediaz78940@gmail.com>
2021-12-13 18:08:58 -0400

    ramdon de los dos dados

--- Main.py
+++ Main.py
@@ -6,7 +6,15 @@ def main():
     print("\n=====================")
     print("\nRealizado por Jose Diaz y Ashly Scarpati")
     print("\n=====================")
-
+    sw = "1"
+    while sw=="1":        
+        for i in range(1,3):            
+            random.seed(None)
+            resultado = random.randint(1, 6)
+            print("el resultado de su", i,"dado es ", resultado)
+            
+        print("Ingrese 1 si desea lanzar otro par de dados de seis lados")
+        sw = input()
 
 
 if __name__ == "__main__":
