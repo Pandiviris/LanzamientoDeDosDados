211ff33b3b8e892242b7f4c7dcc3f80fdc8b81f6
Pandiviris <josediaz78940@gmail.com>
2021-12-13 18:02:50 -0400

    creado main

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
 #Simulando lanzamiento de dos dados. Realizado por Jose Diaz y Ashly Scarpati. Simulacion y modelos UDONE
+import random
+
+def main():
+    print("\nSimulando lanzamiento de dos dados de seis lados")
+    print("\n=====================")
+    print("\nRealizado por Jose Diaz y Ashly Scarpati")
+    print("\n=====================")
+
+
+
+if __name__ == "__main__":
+    main()
